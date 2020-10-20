818eff7bf1b759a4a5d769716539895bfead40d0
Emma <emma.cebada@gmail.com>
2020-10-20 21:35:26 +0200

    Add issues almost resolved, just one left

--- main.js
+++ main.js
@@ -6,34 +6,36 @@ const clue = document.querySelector(".js-clue");
 const tries = document.querySelector(".js-tries");
 
 //Para que se produzca el número aleatorio.
-const getRandom = function getRandomNumber(max) {
+function getRandomNumber(max) {
   return Math.ceil(Math.random() * max);
-};
-function toClick() {
-  const value = numberTest.value;
-  numberTest.innerHTML = value;
-  const random = getRandom();
-  console.log(random);
-  if (numberTest < random) {
-    console.log("Demasiado bajo."); 
-  } else if (numberTest > ramdom) {
-    console.log("Demasiado alto.") 
-  } else if (numberTest === ramdom) {
-    console.log("Has ganado campeona!!!")
-  } else if (numberTest > 100) {
-    console.log("El número debe estar entre 1 y 100.")
-  } else if (numberTest < 1) {
-    console.log("El número debe estar entre 1 y 100.")
-  }
 }
 
-button.addEventListener("click", toClick);
- 
+const getRandom = getRandomNumber(100);
+console.log(getRandom);
+
+function toClick() {
+  let inputValue = parseInt(numberTest.value);
+  if (inputValue < getRandom) {
+    clue.innerHTML = "Demasiado bajo.";
+  } else if (inputValue > getRandom) {
+    clue.innerHTML = "Demasiado alto.";
+  } else if (inputValue === getRandom) {
+    clue.innerHTML = "Has ganado campeona!!!";
+  } else if (inputValue > 100) {
+    clue.innerHTML = "El número debe estar entre 1 y 100.";
+  } else if (inputValue < 1) {
+    clue.innerHTML = "El número debe estar entre 1 y 100.";
+  }
+  manyTimes();
+  tries.innerHTML += `Número de intentos: ${manyTimes}`;
+}
 
 //Para que recoja el número que introduce la usuaria.
 
-function manyTimes(event) {
-  const times = document.querySelector(".js-numberTest");
-  let input = event.currentTarget.value;
-  console.log(input);
+let manyclicks = 0;
+function manyTimes() {
+  manyclicks++;
+  return manyclicks;
 }
+
+button.addEventListener("click", toClick);
